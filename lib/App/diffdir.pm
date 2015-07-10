fb5c53875b0be94e3142f70f8a0ffe3590afc67b
Ivan Wills <ivan.wills@gmail.com>
2015-07-10 16:47:29 +1000

    Updating for release of version 0.3

--- lib/App/diffdir.pm
+++ lib/App/diffdir.pm
@@ -12,7 +12,7 @@ use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 use base qw/Exporter/;
 
-our $VERSION = 0.2;
+our $VERSION = 0.3;
 
 1;
 
@@ -24,7 +24,7 @@ App::diffdir - Compares two or more directories for files that differ
 
 =head1 VERSION
 
-This documentation refers to App::diffdir version 0.2
+This documentation refers to App::diffdir version 0.3
 
 =head1 SYNOPSIS
 
